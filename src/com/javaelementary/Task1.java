a0de6677ac1421e0dab6ad2e0c30a70b65fda7a3
Mariia Pasichna <pasichnayam@gmail.com>
2020-02-21 14:53:05 +0200

    correct

--- src/com/javaelementary/Task1.java
+++ src/com/javaelementary/Task1.java
@@ -1,8 +1,5 @@
 package com.javaelementary;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class Task1 {
 
     /*
@@ -21,54 +18,27 @@ public class Task1 {
     }
 
     private static boolean backHome(String robotRoute) {
-        Deque<Character> deque = new LinkedList<>();
+        int x = 0;
+        int y = 0;
         for (int i = 0; i < robotRoute.length(); i++) {
             char ch = robotRoute.charAt(i);
-            if (deque.isEmpty()) {
-                deque.addFirst(ch);
-            } else {
-                switch (ch) {
-                    case 'R':
-                        if (deque.peekFirst() == 'L') {
-                            deque.pollFirst();
-                        } else if (deque.peekLast() == 'L') {
-                            deque.pollLast();
-                        } else {
-                            deque.addFirst(ch);
-                        }
-                        break;
-                    case 'L':
-                        if (deque.peekFirst() == 'R') {
-                            deque.pollFirst();
-                        } else if (deque.peekLast() == 'R') {
-                            deque.pollLast();
-                        } else {
-                            deque.addFirst(ch);
-                        }
-                        break;
-                    case 'U':
-                        if (deque.peekFirst() == 'D') {
-                            deque.pollFirst();
-                        } else if (deque.peekLast() == 'D') {
-                            deque.pollLast();
-                        } else {
-                            deque.addFirst(ch);
-                        }
-                        break;
-                    case 'D':
-                        if (deque.peekFirst() == 'U') {
-                            deque.pollFirst();
-                        } else if (deque.peekLast() == 'U') {
-                            deque.pollLast();
-                        } else {
-                            deque.addFirst(ch);
-                        }
-                        break;
-                    default:
-                        throw new RuntimeException("Unexpected char: " + ch);
-                }
+            switch (ch) {
+                case 'R':
+                    y++;
+                    break;
+                case 'L':
+                    y--;
+                    break;
+                case 'U':
+                    x++;
+                    break;
+                case 'D':
+                    x--;
+                    break;
+                default:
+                    throw new RuntimeException("Unexpected char: " + ch);
             }
         }
-        return deque.isEmpty();
+        return x == 0 && y == 0;
     }
 }
